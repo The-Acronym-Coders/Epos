01dce77e2d22318b0cf0bd5d6689c1c811c4ae70
Lanse <simon_stalnabb@hotmail.com>
2019-07-15 22:00:47 +0200

    Started work on new sound things

---
+++ src/api/java/com/teamacronymcoders/eposmajorum/api/sounds/SoundEvents.java
@@ -0,0 +1,9 @@
+package com.teamacronymcoders.eposmajorum.api.sounds;
+
+import com.teamacronymcoders.eposmajorum.api.EposAPI;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+public class SoundEvents {
+    public static final SoundEvent levelUp = new SoundEvent(new ResourceLocation(EposAPI.ID, "level-up"));
+}
