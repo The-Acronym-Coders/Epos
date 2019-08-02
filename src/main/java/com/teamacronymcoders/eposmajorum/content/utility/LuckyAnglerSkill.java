bf35c7f38b695791f946d56350b6448748667b4c
Lanse <simon_stalnabb@hotmail.com>
2019-08-03 00:59:01 +0200

    Added Javadoc for Clarity

--- src/main/java/com/teamacronymcoders/eposmajorum/content/utility/LuckyAnglerSkill.java
+++ src/main/java/com/teamacronymcoders/eposmajorum/content/utility/LuckyAnglerSkill.java
@@ -11,6 +11,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
+/**
+ * You might look at this in the future and look at the "Math.max()" call and wonder "Why?"
+ * Well I'll leave this Quote from the Official Minecraft Wiki to explain why we have to have a Max of 5 on the "Lure" enchantment.
+ *
+ * Lure increases the rate of a fish biting the player's hook while fishing. Specifically, it decreases the wait time for a fish to appear (by 5 seconds per level).
+ * If commands are used to increase the enchantment level beyond 5, the wait timer does not start and fish do not bite.
+ */
 public class LuckyAnglerSkill {
     private static final ResourceLocation NAME = new ResourceLocation(EposAPI.ID, "lucky_angler");
     public static final Feat FEAT =
