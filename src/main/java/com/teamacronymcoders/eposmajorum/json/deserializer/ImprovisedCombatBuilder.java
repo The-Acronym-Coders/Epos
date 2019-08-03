fbffe0c5a93cd66d7be26f0980a81365aa214195
Lanse <simon_stalnabb@hotmail.com>
2019-08-03 15:00:41 +0200

    Kill the useless!

--- src/main/java/com/teamacronymcoders/eposmajorum/json/deserializer/ImprovisedCombatBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.teamacronymcoders.eposmajorum.json.deserializer;
-
-import net.minecraft.item.Item;
-
-import java.util.List;
-
-public class ImprovisedCombatBuilder {
-    private List<Item> items;
-}
