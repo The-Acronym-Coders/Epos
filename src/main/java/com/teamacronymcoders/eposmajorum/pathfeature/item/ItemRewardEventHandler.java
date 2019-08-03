c49e77560d2cbfc4b5eb027a1270cb5e5d488f2f
Lanse <simon_stalnabb@hotmail.com>
2019-08-03 16:42:05 +0200

    Added params to Annotation

--- src/main/java/com/teamacronymcoders/eposmajorum/pathfeature/item/ItemRewardEventHandler.java
+++ src/main/java/com/teamacronymcoders/eposmajorum/pathfeature/item/ItemRewardEventHandler.java
@@ -1,10 +1,11 @@
 package com.teamacronymcoders.eposmajorum.pathfeature.item;
 
+import com.teamacronymcoders.eposmajorum.api.EposAPI;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = EposAPI.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ItemRewardEventHandler {
     public static void onDeath(PlayerEvent.Clone event) {
         if (event.getOriginal().getEntityData().contains("item_rewards")) {
