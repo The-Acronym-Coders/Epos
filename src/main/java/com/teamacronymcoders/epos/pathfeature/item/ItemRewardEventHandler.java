bdf24e96e2b33fd491b6ad6658d7600e68191bbd
Lanse <simon_stalnabb@hotmail.com>
2019-08-09 17:31:01 +0200

    TODO for ItemRewardEventHandler

--- src/main/java/com/teamacronymcoders/epos/pathfeature/item/ItemRewardEventHandler.java
+++ src/main/java/com/teamacronymcoders/epos/pathfeature/item/ItemRewardEventHandler.java
@@ -5,6 +5,7 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 
+// TODO: Replace with Capability
 @Mod.EventBusSubscriber(modid = EposAPI.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ItemRewardEventHandler {
     public static void onDeath(PlayerEvent.Clone event) {
