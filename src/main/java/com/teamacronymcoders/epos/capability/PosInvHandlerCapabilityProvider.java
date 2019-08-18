9b7fde5fe659635e47c58a8664183cf6dc49effc
Lanse <simon_stalnabb@hotmail.com>
2019-08-18 21:52:35 +0200

    Fixed NBT Syncing

--- src/main/java/com/teamacronymcoders/epos/capability/PosInvHandlerCapabilityProvider.java
+++ src/main/java/com/teamacronymcoders/epos/capability/PosInvHandlerCapabilityProvider.java
@@ -2,17 +2,20 @@ package com.teamacronymcoders.epos.capability;
 
 import com.hrznstudio.titanium.block.tile.inventory.PosInvHandler;
 import net.minecraft.item.ShootableItem;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Objects;
 
-public class PosInvHandlerCapabilityProvider implements ICapabilityProvider {
+public class PosInvHandlerCapabilityProvider implements ICapabilityProvider, INBTSerializable<CompoundNBT> {
     private PosInvHandler handler = new PosInvHandler("quiver", 62, 30, 9)
             .setInputFilter((idStack, integer) -> ShootableItem.ARROWS_OR_FIREWORKS.test(idStack))
             .setRange(3, 3);;
@@ -25,5 +28,19 @@ public class PosInvHandlerCapabilityProvider implements ICapabilityProvider {
             return posInv.cast();
         }
         return LazyOptional.empty();
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        CompoundNBT nbt = new CompoundNBT();
+        if (handler != null) {
+            nbt.put("items", Objects.requireNonNull(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.writeNBT(handler, null)));
+        }
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.readNBT(handler, null, nbt.get("items"));
     }
 }
