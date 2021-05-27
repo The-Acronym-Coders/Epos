1530894c7712c4931fdb1ee6929b03f18e3cb4ce
Lanse <simon_stalnabb@hotmail.com>
2021-05-27 18:05:41 +0200

    Comment out broken classes

--- src/main/java/com/teamacronymcoders/epos/json/path/CharacterClassJsonLoader.java
+++ src/main/java/com/teamacronymcoders/epos/json/path/CharacterClassJsonLoader.java
@@ -1,9 +1,9 @@
 package com.teamacronymcoders.epos.json.path;
 
 import com.google.common.collect.Lists;
-import com.hrznstudio.titanium.json.IJsonProvider;
-import com.hrznstudio.titanium.json.JsonLoader;
-import com.hrznstudio.titanium.json.jsondirector.IJsonDirector;
+//import com.hrznstudio.titanium.json.IJsonProvider;
+//import com.hrznstudio.titanium.json.JsonLoader;
+//import com.hrznstudio.titanium.json.jsondirector.IJsonDirector;
 import com.teamacronymcoders.epos.api.EposAPI;
 import com.teamacronymcoders.epos.api.registry.IRegistryEntry;
 import com.teamacronymcoders.epos.api.registry.RegistrationEvent;
@@ -26,66 +26,66 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class CharacterClassJsonLoader<T extends IRegistryEntry> extends JsonLoader<T> implements ISelectiveResourceReloadListener {
-    private final String type;
-    private final IResourceType resourceType;
-    private final Class<T> tClass;
-    private final Registry<T> registry;
-
-    public CharacterClassJsonLoader(IResourceType resourceType, Class<T> tClass, Registry<T> registry, IJsonDirector<T> director, IJsonProvider<T> jsonProvider) {
-        super("character_class", EposAPI.LOGGER, director, jsonProvider);
-        this.type = "character_class";
-        this.resourceType = resourceType;
-        this.tClass = tClass;
-        this.registry = registry;
-    }
-
-    @Override
-    public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
-        if (resourcePredicate.test(this.getResourceType())) {
-            registry.clear();
-            RegistrationEvent<T> registryEvent = new RegistrationEvent<>(tClass, registry);
-            List<T> pathList = new ArrayList<>();
-            registryEvent.register(loadValues(resourceManager));
-            MinecraftForge.EVENT_BUS.post(registryEvent);
-        }
-    }
-
-    private List<T> loadValues(IResourceManager resourceManager) {
-        final String folder = "epos/" + type;
-        final String extension = ".json";
-        return resourceManager.getAllResourceLocations(folder, n -> n.endsWith(extension))
-            .stream()
-            .map(resource -> new ResourceLocation(resource.getNamespace(), resource.getPath().substring(folder.length() + 1, resource.getPath().length() - extension.length())))
-            .map(resource -> {
-                try {
-                    return resourceManager.getAllResources(resource);
-                } catch (IOException exception) {
-                    EposAPI.LOGGER.warn("Failed to Load Files for " + type, exception);
-                    return Lists.<IResource>newArrayList();
-                }
-            })
-            .map(resources -> resources.stream()
-                .map(resource -> {
-                    try {
-                        return gson.fromJson(IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8), tClass);
-                    } catch (IOException e) {
-                        EposAPI.LOGGER.warn("Failed to Parse " + type + " for file: " + resource.getLocation().toString());
-                        return null;
-                    } finally {
-                        IOUtils.closeQuietly(resource);
-                    }
-                    //TODO Proper Merging
-                }).reduce((first, last) -> last == null ? first : last)
-            )
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .collect(Collectors.toList());
-    }
-
-    @Nullable
-    @Override
-    public IResourceType getResourceType() {
-        return resourceType;
-    }
-}
+//public class CharacterClassJsonLoader<T extends IRegistryEntry> extends JsonLoader<T> implements ISelectiveResourceReloadListener {
+//    private final String type;
+//    private final IResourceType resourceType;
+//    private final Class<T> tClass;
+//    private final Registry<T> registry;
+//
+//    public CharacterClassJsonLoader(IResourceType resourceType, Class<T> tClass, Registry<T> registry, IJsonDirector<T> director, IJsonProvider<T> jsonProvider) {
+//        super("character_class", EposAPI.LOGGER, director, jsonProvider);
+//        this.type = "character_class";
+//        this.resourceType = resourceType;
+//        this.tClass = tClass;
+//        this.registry = registry;
+//    }
+//
+//    @Override
+//    public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
+//        if (resourcePredicate.test(this.getResourceType())) {
+//            registry.clear();
+//            RegistrationEvent<T> registryEvent = new RegistrationEvent<>(tClass, registry);
+//            List<T> pathList = new ArrayList<>();
+//            registryEvent.register(loadValues(resourceManager));
+//            MinecraftForge.EVENT_BUS.post(registryEvent);
+//        }
+//    }
+//
+//    private List<T> loadValues(IResourceManager resourceManager) {
+//        final String folder = "epos/" + type;
+//        final String extension = ".json";
+//        return resourceManager.getAllResourceLocations(folder, n -> n.endsWith(extension))
+//            .stream()
+//            .map(resource -> new ResourceLocation(resource.getNamespace(), resource.getPath().substring(folder.length() + 1, resource.getPath().length() - extension.length())))
+//            .map(resource -> {
+//                try {
+//                    return resourceManager.getAllResources(resource);
+//                } catch (IOException exception) {
+//                    EposAPI.LOGGER.warn("Failed to Load Files for " + type, exception);
+//                    return Lists.<IResource>newArrayList();
+//                }
+//            })
+//            .map(resources -> resources.stream()
+//                .map(resource -> {
+//                    try {
+//                        return gson.fromJson(IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8), tClass);
+//                    } catch (IOException e) {
+//                        EposAPI.LOGGER.warn("Failed to Parse " + type + " for file: " + resource.getLocation().toString());
+//                        return null;
+//                    } finally {
+//                        IOUtils.closeQuietly(resource);
+//                    }
+//                    //TODO Proper Merging
+//                }).reduce((first, last) -> last == null ? first : last)
+//            )
+//            .filter(Optional::isPresent)
+//            .map(Optional::get)
+//            .collect(Collectors.toList());
+//    }
+//
+//    @Nullable
+//    @Override
+//    public IResourceType getResourceType() {
+//        return resourceType;
+//    }
+//}
--- src/main/java/com/teamacronymcoders/epos/json/path/CharacterClassJsonProvider.java
+++ src/main/java/com/teamacronymcoders/epos/json/path/CharacterClassJsonProvider.java
@@ -2,17 +2,17 @@ package com.teamacronymcoders.epos.json.path;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.hrznstudio.titanium.json.IJsonProvider;
+//import com.hrznstudio.titanium.json.IJsonProvider;
 import com.teamacronymcoders.epos.classes.CharacterClass;
-import com.teamacronymcoders.epos.path.Path;
+//import com.teamacronymcoders.epos.path.Path;
 import net.minecraft.util.ResourceLocation;
 
-public class CharacterClassJsonProvider implements IJsonProvider<CharacterClass> {
-
-    @Override
-    public CharacterClass provide(ResourceLocation resourceLocation, JsonObject jsonObject) throws JsonParseException {
-
-        return null;
-    }
-
-}
+//public class CharacterClassJsonProvider implements IJsonProvider<CharacterClass> {
+//
+//    @Override
+//    public CharacterClass provide(ResourceLocation resourceLocation, JsonObject jsonObject) throws JsonParseException {
+//
+//        return null;
+//    }
+//
+//}
